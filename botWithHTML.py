bdb2069099e6773f9241cbd613d0fa7f6910c7bd
5anku <apn712@uowmail.edu.au>
2024-04-02 14:48:32 +0100

    Initial commit

---
+++ botWithHTML.py
@@ -0,0 +1,77 @@
+import random
+import json
+import pickle
+import numpy as np
+import tensorflow as tf
+from flask import Flask, request, jsonify, render_template
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import load_model
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbot_model.h5')
+
+app = Flask(__name__)
+
+context = None
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
+
+def bag_of_words(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+def get_response(intents_list, intents_json):
+    global context
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            if 'context_set' in i:
+                context = i['context_set']
+                print("Expecting an answer for", context)
+            result = random.choice(i['responses'])
+            break
+    return result
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/get_response', methods=['POST'])
+def get_bot_response():
+    global context
+    message = request.form['message']
+    print("Context:", context)
+    ints = predict_class(message)
+    res = get_response(ints, intents)
+    return res
+
+if __name__ == '__main__':
+    app.run(debug=True)
